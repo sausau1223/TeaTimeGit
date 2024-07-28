3cf48babddbaa81f34f11436583aedd3cbd9069c
sausau1223 <70872068+sausau1223@users.noreply.github.com>
2024-07-28 19:08:46 +0800

    刪除專案

--- TeaTimeDemo.Models/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace TeaTimeDemo.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
